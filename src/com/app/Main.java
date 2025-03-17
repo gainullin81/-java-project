bed603b0bf105fd71eb6e966cf9b0bbdd48bfea3
alex <againullin8@gmail.com>
2025-03-18 01:45:49 +0200

    Add Java project structure and main files

---
+++ src/com/app/Main.java
@@ -0,0 +1,10 @@
+package com.app;
+
+import com.utils.Helper;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+        Helper.printMessage("This is a helper message.");
+    }
+}
---
+++ src/com/utils/Helper.java
@@ -0,0 +1,7 @@
+package com.utils;
+
+public class Helper {
+    public static void printMessage(String message) {
+        System.out.println("Helper says: " + message);
+    }
+}
